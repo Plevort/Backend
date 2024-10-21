6609400700fa8e149b0463a1aaaebaf61e97e51d
GGLVXD <gglvmax@gmail.com>
2024-10-21 16:34:51 +0300

    fix cors

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
-// /main.js
-const fastify = require('fastify')({ logger: true })
+const fastify = require('fastify')({ logger: true });
+const fastifyCors = require('@fastify/cors');
 const connectmongodb = require('./mongodb.js');
 
-// Connect to mongodb
+// Connect to MongoDB
 connectmongodb();
 
-// /
+fastify.register(fastifyCors, {
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true 
+});
+
+// / (root)
 const RootRoute = require('./v1/root/root.js');
 fastify.register(RootRoute);
 
-// /v1/register
+// /v1/register (auth)
 const RegisterRoute = require('./v1/auth/register.js');
 fastify.register(RegisterRoute);
 
 // Run the server
 fastify.listen({ port: 3000 }, (err) => {
   if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-})
+});
